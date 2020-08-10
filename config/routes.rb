817367c6d28c29b8cd7901dd35aef8631cd4fe06
ryosuke7651 <68171088+ryosuke7651@users.noreply.github.com>
2020-08-10 14:55:35 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'   
-  resources :users, only: [:edit, :update]
-  resources :groups, only: [:inedx, :new, :create, :edit, :update]
+  resources :users, only: [:edit, :update, :index]
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
+    namespace :api do
+      resources :messages, only: :index, defaults: { format: 'json' }
+    end
+  end  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
